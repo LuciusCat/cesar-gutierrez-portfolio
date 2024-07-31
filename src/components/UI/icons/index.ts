482ad8b93204eb41128f82331d75545bdaa6e702
César Gutiérrez <cesar.antonio.g_555@hotmail.com>
2024-07-31 06:51:52 -0500

    agregando projects y aboutme

---
+++ src/components/UI/icons/index.ts
@@ -0,0 +1,27 @@
+import Angular from "./Angular.astro";
+import React from "./React.astro";
+import AstroF from "./AstroF.astro";
+import CSS from "./CSS.astro";
+import Git from "./Git.astro";
+import Html5 from "./Html5.astro";
+import Javascript from "./Javascript.astro";
+import NodeJS from "./NodeJS.astro";
+import Java from "./Java.astro";
+import Github from "./Github.astro";
+import Tailwind from "./Tailwind.astro";
+import Typescript from "./Typescript.astro";
+
+export {
+  Angular,
+  React,
+  AstroF,
+  CSS,
+  Git,
+  Html5,
+  Javascript,
+  NodeJS,
+  Java,
+  Github,
+  Tailwind,
+  Typescript,
+};
